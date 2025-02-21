0b4aadfd33bc06c01aa9db9390ee990fc8cd450a
Helton Ellison <heltonellison@yahoo.com>
2025-02-20 20:58:36 -0500

    updated css to sass, separated form and tasks into two different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,78 @@
+import { useState } from "react";
+import "./App.scss";
 
-import "./App.css";
+const TaskForm = () => {
+    const [formData, setFormData] = useState({
+        task: "",
+    });
 
-function App() {
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:4000/submit-form", {
+                method: "POST",
+                headers: {
+                    "content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                console.log("sucessful submit");
+            }
+        } catch (error) {
+            console.log("Something went wrong", error);
+        }
+    };
+
     return (
-        
-            <div id="centerContainer">
-                <form id="formContainer">
-                    <div class="Title">Enter Tasks</div>
+        <>
+            <div id="formContainer" className="formContainer">
+                <form id="form" onSubmit={handleSubmit}>
+                    <div className="Title">Enter Tasks</div>
                     <input
-                        id="myTask"
+                        id=""
                         type="text"
-                        name="myTask"
+                        name="task"
                         placeholder="Enter a task"
+                        value={formData.task}
+                        onChange={handleChange}
                     />
-                    <div id="msg"></div>
+
                     <button id="addTask" type="submit">
-                        {" "}
                         Add Task
                     </button>
                 </form>
+            </div>
+        </>
+    );
+};
 
-                <div id="tasksContainer">
-                    <div class="Title">Task List </div>
-                    <div>
-                        <p>task 1 to complete</p>
-                        <span class="options">
-                            <i class="fa-solid fa-file-pen"></i>
-                            <i class="fa-solid fa-trash"></i>
-                        </span>
-                    </div>
-                    <div>
-                        <p>task 2 to complete</p>
-                        <span class="options">
-                            <i class="fa-solid fa-file-pen"></i>
-                            <i class="fa-solid fa-trash"></i>
-                        </span>
-                    </div>
+const Tasks = () => {
+    return (
+        <>
+            <div className="tasksContainer">
+                <div className="title">Task List </div>
+                <div className="tasks">
+                    <p>task 1 to complete</p>
+                    <p>task 2 to complete</p>
                 </div>
             </div>
-    
+        </>
+    );
+};
+
+function App() {
+    return (
+        <div className ="App">
+            <TaskForm/>
+            <Tasks/>
+        </div>
     );
 }
 
